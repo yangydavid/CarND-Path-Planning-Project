1de2853377bed6d835c36b18722f580b51743d68
davidyy <david.yang@byton.com>
2019-06-02 13:29:50 -0700

    simple lane change

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -50,8 +51,14 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
-               &map_waypoints_dx,&map_waypoints_dy]
+  // Define the order of lanes
+  int lane = 1; 
+
+  // Define the reference of velocity 
+  double ref_v = 0;  // mph
+
+  h.onMessage([&ref_v, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
+               &map_waypoints_dx,&map_waypoints_dy, &lane]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -98,6 +105,155 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+          int prev_size = previous_path_x.size(); 
+
+          /*
+          * -------------------------------SENSOR FUSION------------------------------------------ 
+          * 1. we will look around the ego vehicle, and check the status of other vehicles on the road
+          */
+
+          // re-assign value to car_s if previous path was not empty 
+          if (prev_size > 0 ){
+            car_s = end_path_s; 
+          }
+
+          // define a flag for closeness check 
+          bool too_close = false; 
+
+          // loop over the sensor function result to check other vehicle's behavior 
+          for (int i = 0; i < sensor_fusion.size(); i++){
+              // check if the car is current in my lane
+              // | --- | --- | --- | (each | represets a lane, in our case, we use the range of d to determine the lane position)
+              float d = sensor_fusion[i][6]; 
+              if (d > (2+4*lane-2) && d < (2+4*lane+2)){
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx + vy*vy); 
+                double check_car_s = sensor_fusion[i][5];   
+
+                check_car_s += (double)prev_size*0.02*check_speed; 
+
+                if (check_car_s > car_s && check_car_s-car_s < 30){
+                  too_close = true; 
+                  if (lane > 0) lane--; 
+                }
+
+              }
+          }
+
+          if (too_close){
+            ref_v -= 0.224;
+          }
+          else if (ref_v < 49.5){
+            ref_v += 0.224; 
+          }
+
+          // we first created some widely spread (x,y) waypoints to generate an outline, and later
+          // using a spline to interpolate the waypoints in between. 
+          vector<double> ptsx; 
+          vector<double> ptsy; 
+
+          // define the reference of the vehicle for all states, x, y and yaw
+          // the reference values could either be the starting point of the vehicle, or the end point of last path. 
+          double ref_x = car_x;
+          double ref_y = car_y; 
+          double ref_yaw = deg2rad(car_yaw); 
+
+          // if the previous path is almost empty, we use the car's starting point as reference
+          if (prev_size < 2){
+            // assume the two consecutive points have the same yaw angle. 
+            double prev_car_x = car_x - cos(ref_yaw); 
+            double prev_car_y = car_y - sin(ref_yaw); 
+
+            // push back both current and starting trajectory point 
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y); 
+          }else{ // using the previous trajectory's end point as reference
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1]; 
+
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2]; 
+            ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev); 
+
+            // push the points back to the waypoints list 
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(ref_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(ref_y); 
+          }
+
+          // In frenet frame, add points to the path roughly 30 meters to have a previw horizon of the trajectory
+          vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y); 
+          vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y); 
+          
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          // Then, for each point in the trajectory list, we convert it into local frame. 
+          for (int i = 0; i<ptsx.size(); i++){
+            // shift the cars location from global coordinates to local coordinates 
+            double shift_x = ptsx[i] - ref_x;
+            double shift_y = ptsy[i] - ref_y; 
+
+            ptsx[i] = shift_x*cos(0-ref_yaw) - shift_y*sin(0-ref_yaw);
+            ptsy[i] = shift_x*sin(0-ref_yaw) + shift_y*cos(0-ref_yaw);  
+          }
+
+          // Then, we create a spline to interpolate the waypoints between each sparse waypoints 
+          tk::spline s; 
+
+          // Set waypoints created to the spline
+          s.set_points(ptsx,ptsy);
+          
+          // Start with all the previous waypoint from previous published trajectory
+          for (int i=0; i < prev_size; i++){
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          // we then calculated how we split our waypoints in order to traveral at desired speed
+          double target_x = 30;  
+          double target_y = s(target_x); 
+          double target_dist = sqrt(pow(target_x,2) + pow(target_y,2)); 
+
+          // define an x value incrementer 
+          double x_add_on = 0; 
+
+          // Fill the rest of the waypoints
+          for (int i=0; i < 50-prev_size; i++){
+            double N = (target_dist/(0.02*ref_v/2.24)); 
+            double x_point = x_add_on + target_dist/N; 
+            double y_point = s(x_point);
+
+            x_add_on = x_point; 
+
+            double x_ref = x_point; 
+            double y_ref = y_point; 
+
+            // rotate the coordinates back to the global frame
+            x_point = (x_ref*cos(ref_yaw) - y_ref*sin(ref_yaw));
+            y_point = (x_ref*sin(ref_yaw) + y_ref*cos(ref_yaw)); 
+
+            // add on the distance between the vehicle and the origin of the world map
+            x_point += ref_x; 
+            y_point += ref_y; 
+
+            // push back the interpolated waypoints 
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point); 
+          }
+          
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
